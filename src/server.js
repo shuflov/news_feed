71d86e14570639ddbbc98874d1392f30e5f4b14a
shuflovic <pavel.skuta@gmail.com>
2026-02-24 09:16:28 +0100

    as

--- src/server.js
+++ src/server.js
@@ -23,9 +23,9 @@ app.use(session({
   resave: false,
   saveUninitialized: false,
   cookie: { 
-    secure: true,
+    secure: process.env.NODE_ENV === 'production',
     httpOnly: true,
-    sameSite: 'none',
+    sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
     maxAge: 30 * 24 * 60 * 60 * 1000 
   }
 }));
